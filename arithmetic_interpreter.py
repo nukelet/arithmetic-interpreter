b7d5f56915c147cb308e991e9ba32b402c1aa4e2
@nukelet <vinicius241201@gmail.com>
2020-12-27 06:38:09 -0300

    Add support to parenthesis delimiters

--- arithmetic_interpreter.py
+++ arithmetic_interpreter.py
@@ -1,16 +1,25 @@
+# Vinícius Peixoto, 2020
+# Very simple interpreter for arithmetic expressions, written
+# as a project for educational purposes
+#
+# Based on the first few chapters of this great tutorial 
+# by Ruslan Spivak: https://ruslanspivak.com/lsbasi-part1/
+
 import sys # sys.stderr
 
+# types
 INTEGER = 'INTEGER'
-PLUS, MINUS, TIMES, DIVIDE = 'PLUS', 'MINUS', 'TIMES', 'DIVIDE' 
-EOF = 'EOF'
-OPERATOR = 'OPERATOR'
-operations = {
-    '+' : PLUS,
-    '-' : MINUS,
-    '*' : TIMES,
-    '/' : DIVIDE
-}
 
+# operators
+PLUS, MINUS, TIMES, DIVIDE = 'PLUS', 'MINUS', 'TIMES', 'DIVIDE' 
+OPERATOR = 'OPERATOR'
+
+# delimiters
+L_PAR = 'L_PAR'
+R_PAR = 'R_PAR'
+EOF = 'EOF'
+
+# class representing a basic token object Token(TYPE, VALUE)
 class Token:
     def __init__(self, type, value):
         self.type = type
@@ -22,6 +31,8 @@ class Token:
     def __repr__(self):
         return self.__str__()
 
+# lexer class that extracts tokens from a string
+# returns a list of tokens through the get_tokens() method
 class Lexer:
     def __init__(self, text):
         self.text = text
@@ -52,21 +63,22 @@ class Lexer:
     
         return int(n)
 
-    # def read_str(self):
-    #     s = ''
-    #     while self.current_char is not None and not (self.current_char.isspace()):
-    #         s += self.current_char
-    #         self.advance()
-    #     return s
-
     def get_tokens(self):
         while self.current_char is not None:
             
             if self.current_char.isspace():
                 self.skip_whitespaces()
 
-            if self.current_char.isdigit():
+            elif self.current_char.isdigit():
                 self.tokens.append(Token(INTEGER, self.read_int()))
+
+            elif self.current_char == '(':
+                self.tokens.append(Token(L_PAR, '('))
+                self.advance()
+
+            elif self.current_char == ')':
+                self.tokens.append(Token(R_PAR, ')'))
+                self.advance()
 
             else:
                 # self.tokens.append(Token(OPERATOR, self.read_str()))
@@ -76,7 +88,12 @@ class Lexer:
         self.tokens.append(Token(EOF, 'EOF'))
         return self.tokens
 
-
+# class that evaluates operations between tokens
+#
+# at first the main idea here was to allow easy
+# implementation of new operations between tokens, but
+# this needs some tweaking/refactoring due to the way that
+# operator precedence is implemented in the Interpreter
 class Evaluator:
     # all operators currently receive two tokens
     def PLUS(self, a, b):
@@ -118,8 +135,14 @@ class Evaluator:
         else:
             return self.operations[operation.value](a, b)
 
-# simple interpreter that implements a single grammar rule:
-# (INTEGER) (OPERATOR) (INTEGER)
+# very simple and barebones recursive descent parser/interpreter
+# for arithmetic expressions involving the four basic operations (+, -, *, /);
+# also supports parentheses
+# 
+# it's conceptually implemented as a parser for the following context-free grammar:
+# expr = term ((TIMES | DIVIDE) term)*      // higher precedency level, for * and /
+# term = factor ((PLUS | MINUS) factor)*    // precedency level for + and -
+# factor = INTEGER | L_PAR expr R_PAR       // recursive step here
 class Interpreter:
     def __init__(self, text):
         self.lexer = Lexer(text)
@@ -132,17 +155,18 @@ class Interpreter:
         # at least an EOF token
         self.current_token = self.tokens[0]
 
+        # keeps track of parentheses and makes sure not
+        # to let stuff like "3+2)" be parsed
+        self.par_stack = []
+
     def error(self, err_token):
         err = ""
         for token in self.tokens:
-            if token.type == EOF:
-                continue
-
             if token != err_token:
-                err += f"{str(token.value)} "
+                err += f"{str(token.value) if token.value != 'EOF' else ''} "
 
             else:
-                err += f"[{str(token.value)}] "
+                err += f"[{str(token.value) if token.value != 'EOF' else ''}] "
 
         raise Exception(f"Error parsing expression: {err.strip()}")
 
@@ -160,7 +184,18 @@ class Interpreter:
         return self.evaluator.evaluate(operation, a, b)
 
     def factor(self):
-        return self.eat(INTEGER)
+        if self.current_token.type == INTEGER:
+            return self.eat(INTEGER)
+        
+        # recursive descent step
+        elif self.current_token.type == L_PAR:
+            self.eat(L_PAR)
+            self.par_stack.append('(')
+            result = self.expr()
+            self.eat(R_PAR)
+            self.par_stack.pop()
+            return result
+
 
     # term = factor ((TIMES | DIVIDE) factor)*
     def term(self):
@@ -191,14 +226,23 @@ class Interpreter:
         # if result is None:
         #     return None
 
-        if self.current_token.type != EOF:
+        if self.current_token.type == R_PAR:
+            # cool trick: for sequences (strings, lists, tuples)
+            # empty sequences are False, whereas non-empty sequences
+            # are True
+            #
+            # very "pythonic!"
+            if not self.par_stack:
+                self.error(self.current_token)
+
+        elif self.current_token.type != EOF:
             self.error(self.current_token)
 
-        return result.value
+        return result
 
 def main():
     while True:
-        text = input()
+        text = input("> ")
 
         # for empty expressions
         if not text:
@@ -211,7 +255,7 @@ def main():
 
         try:
             result = interpreter.expr()
-            print(result)
+            print(result.value)
 
         except Exception as e:
             print("")
